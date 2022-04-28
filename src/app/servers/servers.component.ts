61d7e4257a3a0203e31ee45a8930f239deb51289
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-04-28 11:37:11 -0600

    learned how to dosiplay components using lists and ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   serverName = 'Test Server';
   serverCreated = false;
   userName = '';
+  servers = ['Test', 'Test 2', 'Test 3'];
 
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created. Name is ' + this.serverName;
   }
